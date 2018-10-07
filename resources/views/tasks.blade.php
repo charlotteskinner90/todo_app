b03549bc0668803f9557bfcdca0a5ca431d9905b
charlotteskinner90 <cs.skinner90@hotmail.co.uk>
2018-10-07 18:17:55 +0100

    Remove navbar

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -13,27 +13,6 @@
     </script>
 </head>
 <body>
-    <nav class="navbar">
-        <div class="navbar-brand">
-            <div class="navbar-item">
-                <p class="control">
-                    <a class="button is-primary" href="{{ url('/')}}">
-                            <span class="icon">
-                                <i class="fa fa-calendar"></i>
-                            </span>
-                        <span>Date</span>
-                    </a>
-                </p>
-            </div>
-        </div>
-        <div class="navbar-menu">
-            <div class="navbar">
-                <div class="navbar-item" id="date_picker">
-                    <datepicker v-model="date" input-class="input"></datepicker>
-                </div>
-             </div>
-        </div>
-    </nav>
     <div id='app' class="container">
         <task-list></task-list>
     </div>
